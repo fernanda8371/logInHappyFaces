d6584ca8b69193bff83667cc651658140bfc2c4c
fer <a00837146@tec.mx>
2025-02-18 13:07:08 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// src/App.js
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Login from "./components/Login.js";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Login />} />
+    </Routes>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,23 @@
+// src/components/Navbar.js
+import React from "react";
+import { Link } from "react-router-dom";
+import "../App.css"; // Importa los estilos específicos de la Navbar
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <img src="/happy_faces_logo.png" alt="Happy Faces Logo" className="logo" width="170" height="55" />
+      <div className="nav-links">
+        <Link to="/practice">Practice</Link>
+        <Link to="/explore">Explore</Link>
+        <Link to="/login">Log In</Link>
+        <button className="signup-btn">Sign Up</button>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
+
+/* CSS Ajustado para Responsividad */
+
